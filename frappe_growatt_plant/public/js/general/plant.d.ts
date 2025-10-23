4bcef2e4444b75c2a8e4f6f327553628028fff43
Luigi Azor <146014608+luigiazoreng@users.noreply.github.com>
2025-10-23 20:54:22 +0000

    Remove unused TypeScript configuration and related plant.js functionality

--- frappe_growatt_plant/public/js/general/plant.d.ts
+++
@@ -1,3 +0,0 @@
-import { Item } from "@anygridtech/frappe-types/doctype/erpnext/Item";
-declare function checkMpptRoutine(item_name: string, sn: string): Promise<Item | undefined>;
-export { checkMpptRoutine };
